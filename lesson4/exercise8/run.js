d50fd471b2b99d1599d9ee09a2252ff79e5f71b3
shannen2020 <shannen.lam@gmail.com>
2025-03-01 12:07:18 +0800

    minor edit

--- lesson4/exercise8/run.js
+++ lesson4/exercise8/run.js
@@ -2,7 +2,7 @@ const StockDailyPerformance = [3.1, 2.4, 1.1, 2.8, 4.5, 2.7]
 
 //You have been tasked to create a function called sortPerformanceInAscOrder() 
 // that will return a list of daily performance sorted in ascending order.
-// Write your code below
+
 const sortPerformanceInAscOrder = (stockarray) => stockarray.sort()
 
 // Do not modify this code:
